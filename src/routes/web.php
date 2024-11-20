52f611f0c71f8dbd8d04353d6cdbdf6517b18225
半澤優希 <hanzawa@aris-kk.co.jp>
2024-11-20 12:34:43 +0900

    とりあえずエラーが出なくなったところまで完成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatController;
+use App\Http\Controllers\WebPushController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,8 +12,8 @@ use App\Http\Controllers\ChatController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -21,6 +23,27 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [ChatController::class, 'index'])->name('chat.chat');
 Route::get('/chat', [ChatController::class, 'index'])->name('chat.chat');
 Route::post('/chat', [ChatController::class, 'store'])->name('chat.store');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::view('/webpush', 'webpush');
+
+Route::post('/set-subscription', [WebPushController::class, 'setSubscription'])
+    ->withoutMiddleware(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class);
+
+Route::get('/send', [WebPushController::class, 'send']);
+
+Route::post('/set-subscription', [WebPushController::class, 'setSubscription'])
+->withoutMiddleware(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class);
+
+require __DIR__.'/auth.php';
